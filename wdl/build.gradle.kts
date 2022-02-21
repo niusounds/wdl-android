a6e3559fd29aa5b708dd012d4e3afa450321815f
Yuya Matsuo <niu@niusounds.com>
2022-02-22 01:28:49 +0900

    wdl/build.gradle.kts

---
+++ wdl/build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    id("maven-publish")
+}
+
+group = "com.github.niusounds"
+version = "0.0.1"
+
+android {
+    compileSdk = 31
+
+    defaultConfig {
+        externalNativeBuild {
+            cmake {
+                cppFlags("")
+            }
+        }
+    }
+    externalNativeBuild {
+        cmake {
+            path("src/main/cpp/CMakeLists.txt")
+            version = "3.10.2"
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components.findByName("release"))
+                artifactId = "wdl-android"
+            }
+        }
+    }
+}
